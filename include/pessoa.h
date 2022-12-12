abecd7484916e719ead2a051504b3bfce44c224a
fsd2022 <fsdcsa2004@gmail.com>
2022-12-12 17:47:18 -0300

    Update pessoa.h

--- include/pessoa.h
+++ include/pessoa.h
@@ -1,1 +1,17 @@
-aaaaaaaaaa;
+#ifndef _PESSOA_H_
+#define _PESSOA_H_
+
+#include <string>
+using namespace std;
+
+
+class Pessoa{
+    private:
+        string _nome;
+    public:
+        Pessoa(string nome);
+        string get_nome();
+};
+
+
+#endif
