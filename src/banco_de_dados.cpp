63ef7f03b5f700149aeccc8af6cf481258bf8ecc
joaohenrique0512 <117298108+joaohenrique0512@users.noreply.github.com>
2022-12-14 15:21:44 -0300

    funções de adicionar, atualizar, concluir e cancelar pedidos

--- src/banco_de_dados.cpp
+++ src/banco_de_dados.cpp
@@ -43,6 +43,51 @@ Cadastro Banco_de_dados::efetuar_login(std::string nome){
   return cadastro;
 }
 
+void Banco_de_dados::inserir_pedido(Pedido novo_pedido){
+  BancoDePedidos.push_back(novo_pedido);
+  return;
+}
+
+void Banco_de_dados::alterar_pedido(Pedido pedido_alterado){
+  auto it = BancoDePedidos.begin();
+  bool encontrado = false;
+  for(; it != BancoDePedidos.end(); it++)
+    if(it->get_pedido() == pedido_alterado.get_pedido()){
+      encontrado = true;
+      break;
+    }
+  if(encontrado == false){
+      std::cout << "Pedido não encontrado" << std::endl;
+      return;
+    }
+  std::string atualizacao;
+  std::cout << "Digite a nova situação do pedido:" << std::endl;
+  std::cin >> atualizacao;
+  it->atualiza_estado(atualizacao);
+  return;
+}
+
+void Banco_de_dados::concluir_pedido(Pedido pedido_concluido){
+  std::string concluido = "Pedido concluído";
+  pedido_concluido.atualiza_estado(concluido);
+}
+
+void Banco_de_dados::cancelar_pedido(Pedido pedido_cancelado){
+  auto it = BancoDePedidos.begin();
+  bool encontrado = false;
+  for(; it != BancoDePedidos.end(); it++)
+    if(it->get_pedido() == pedido_cancelado.get_pedido()){
+      encontrado = true;
+      break;
+    }
+  if(encontrado == false){
+      std::cout << "Pedido não encontrado" << std::endl;
+      return;
+    }
+  it->cancela_estado();
+  return;
+}
+
 Cardapio Banco_de_dados::get_cardapio(){
   return _cardapio;
 }
