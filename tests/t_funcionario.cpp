2b108a4cfaaa416438c200af1e340203ba3f29b7
fsd2022 <fsdcsa2004@gmail.com>
2022-12-15 20:04:16 -0300

    testes

--- tests/t_funcionario.cpp
+++ tests/t_funcionario.cpp
@@ -4,4 +4,7 @@
 TEST_CASE("Testa Funcionário"){
     Funcionario func1 = Funcionario("José", "12345");
     CHECK_EQ("José", func1.get_nome());
+    CHECK_EQ("12345", func1.get_senha());
+    CHECK_EQ(1, func1.get_cargo());
+
 }
