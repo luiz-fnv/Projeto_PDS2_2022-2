06fa47c319974ff0aa101f95d719a09f76cc46cf
fsd2022 <fsdcsa2004@gmail.com>
2022-12-20 20:34:21 -0300

    a

--- src/cardapio.cpp
+++ src/cardapio.cpp
@@ -3,31 +3,44 @@
 #include <iostream>
 
 void Cardapio::inserir_produto(){
-  std::cout << "Digite o nome do produto a ser adicionado:\n";
   std::string nome_produto_adicionado;
-  std::getline(std::cin,nome_produto_adicionado);
-  std::cout << "Digite o preco do produto a ser adicionado:\n";
   double preco_produto_adicionado;
+
+  std::cout << "--------------------------------------------------\n";
+  std::cout << "Digite o nome do produto a ser adicionado:\n";
+  std::getline(std::cin,nome_produto_adicionado);
+
+  std::cout << "Digite o preço do produto a ser adicionado (apenas o valor numérico):\n";
   std::cin >> preco_produto_adicionado;
+  std::cin.get();
+
   _cardapio.push_back(Produto(nome_produto_adicionado,preco_produto_adicionado));
-  std::cout << nome_produto_adicionado << " adicionado com sucesso.\n";
+  std::cout << "--------------------------------------------------\n";
+
+  std::cout << "O produto \"" << nome_produto_adicionado << "\" foi adicionado com sucesso.\n";
 }
 
 void Cardapio::remover_produto(){
+  std::string nome_do_produto;
   if(_cardapio.size()==0){
+    std::cout << "--------------------------------------------------\n";
     std::cout << "O cardápio está vazio.\n";
     return;
   }
+
+  std::cout << "--------------------------------------------------\n";
   std::cout << "Digite o nome do produto a ser removido:\n";
-  std::string nome_do_produto;
   std::getline(std::cin,nome_do_produto);
+
   for(unsigned int i=0; i < _cardapio.size(); ++i){
     if(_cardapio.at(i).get_nome() == nome_do_produto){
       _cardapio.erase(_cardapio.begin()+i);
+      std::cout << "--------------------------------------------------\n";
       std::cout << nome_do_produto << " removido com sucesso.\n";
       return;
     }
   }
+  std::cout << "--------------------------------------------------\n";
   std::cout << "Este produto não está no cardápio.\n";
   return;
 }
@@ -39,19 +52,54 @@ std::vector<Produto> Cardapio::get_cardapio(){
 bool sort_nomes(std::string a, std::string b){
   return a<b;
 }
-
 void Cardapio::mostrar_cardapio_alfabetico(){
+  if(_cardapio.size()==0){
+    std::cout << "--------------------------------------------------\n";
+    std::cout << "O cardápio está vazio.\n";
+    return;
+  }
+
   Cardapio temporario;
   temporario._cardapio = _cardapio;
   std::vector<std::string> nomes;
+
   for(unsigned long int i = 0; i < _cardapio.size(); i++){
     nomes.push_back(temporario._cardapio[i].get_nome());
   }
   std::sort(nomes.begin(), nomes.end(), sort_nomes);
+
   for(unsigned long int i = 0; i < nomes.size(); i++){
-    //std::cout << nomes[i] << std::endl;
     for(auto j = _cardapio.begin(); j != _cardapio.end(); j++){
       if(nomes[i] == (*j).get_nome()){
+        std::cout << (*j).descricao_produto();
+      }
+    }
+  }
+}
+
+bool sort_vendas(unsigned int a, unsigned int b){
+  return a<b;
+}
+
+void Cardapio::mostrar_cardapio_mais_vendidos(){
+  if(_cardapio.size()==0){
+    std::cout << "--------------------------------------------------\n";
+    std::cout << "O cardápio está vazio.\n";
+    return;
+  }
+
+  Cardapio temporario;
+  temporario._cardapio = _cardapio;
+  std::vector<unsigned int> num_vendas;
+
+  for(unsigned long int i = 0; i < _cardapio.size(); i++){
+    num_vendas.push_back(temporario._cardapio[i].get_vendidos());
+  }
+  std::sort(num_vendas.begin(), num_vendas.end(), sort_vendas);
+
+  for(unsigned long int i = 0; i < num_vendas.size(); i++){
+    for(auto j = _cardapio.begin(); j != _cardapio.end(); j++){
+      if(num_vendas[i] == (*j).get_vendidos()){
         std::cout << (*j).descricao_produto();
       }
     }
--- src/conta.cpp
+++ src/conta.cpp
@@ -30,8 +30,3 @@ std::string Conta::get_cargo_txt() const{
 int Conta::get_cargo() const{
   return _cargo;
 }
-
-// void Conta::mostrar_cardapio_mais_vendidos(Cardapio cardapio) const{
-//   std::cout << "Lista dos produtos do cardápio ordenados pelo número de vendas:\n";
-//   std::cout << cardapio.cardapio_mais_vendidos();
-// }
